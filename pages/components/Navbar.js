8c8a985f2c14caffc72fb2de831d1d6f9e70a2a4
IceCrystal21 <master0sword@gmail.com>
2026-02-13 18:52:03 -0500

    Started on form component

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -6,9 +6,12 @@ const Navbar = () => {
     const [activePage, setPage] = useState("Home")
 
     return (
-        <nav>
-            <ul>
+        <nav className={styles.navbar}>
+            <ul className={styles.ul}>
                 <li><a href='#' onClick={() => setPage("Home")}>Home</a></li>
+                <li><a href='#' onClick={() => setPage("About")}>About</a></li>
+                <li><a href='#' onClick={() => setPage("Become a mod")}>Become a moderater</a></li>
+                <li><a href='#' onClick={() => setPage("Users")}>Users</a></li>
             </ul>
         </nav>
     );
